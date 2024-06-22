75bef3f5fa5a3747f588ff5138f977172f003c20
Youssef Kandil <youssefkandil@aucegypt.edu>
2024-06-22 21:25:57 +0300

    valid implementation

--- Conv2d.v
+++ Conv2d.v
@@ -16,7 +16,7 @@ module conv #( parameter    DSIZE = 256,
 
     // Input data information
     input   [7:0]               data_width,       // Input data width
-    input   [7:0]               data_hight,       // Input data height
+    input   [7:0]               data_height,       // Input data height
     input   [7:0]               di_x_stop,  // This is the image width - kernel width (to save HW) 
     input   [7:0]               di_y_stop,  // This is the image height - kernel height (to save HW)
 
@@ -28,17 +28,17 @@ module conv #( parameter    DSIZE = 256,
     input   [8*KSIZE*KSIZE-1:0] kernel,          // Convolution Kernel 
                                                  // KSIZE^2 x 8 bits
     input   [3:0]               kernel_width,        
-    input   [3:0]               kernel_hight,        
+    input   [3:0]               kernel_height,        
     
     // Input Data Memory Ports
-    input   [$clog2(DSIZE)-3:0] mi_addr,    // $clog2(DSIZE): the ceiling of the base-2 logarithm of DSIZE, 
+    input   [$clog2(DSIZE):0] mi_addr,    // $clog2(DSIZE): the ceiling of the base-2 logarithm of DSIZE, 
                                             // gives the number of bits needed to address DSIZE unique locations.
                                             // -3: 2^3 = 8  (byte addressable)
     input   [31:0]              mi_data,    // Input data 32 bit word
     input                       mi_wr,
 
     // Output Data Memory Ports
-    input   [$clog2(DSIZE)-3:0] mo_addr,    
+    input   [$clog2(DSIZE):0] mo_addr,    
     output  [31:0]              mo_data,
 
     // Control and status
@@ -65,7 +65,8 @@ module conv #( parameter    DSIZE = 256,
 */
 
     // The Control FSM
-    reg [1:0] state, nstate;
+    reg [1:0] state  =0;
+    reg [1:0] nstate =0;
 
     always @(posedge clk or negedge rst_n)
         if(!rst_n)
@@ -99,11 +100,13 @@ module conv #( parameter    DSIZE = 256,
     assign mo_data = {DO[mo_addr+3], DO[mo_addr+2], DO[mo_addr+1], DO[mo_addr]};
 
     // Address Counters
-    reg[7:0]    di_x_cntr, di_y_cntr;
-    reg[3:0]    k_x_cntr, k_y_cntr;
+    reg[7:0]    di_x_cntr=0;
+    reg[7:0]    di_y_cntr=0;
+    reg[3:0]    k_x_cntr= 0;
+    reg[3:0]    k_y_cntr= 0;
 
     wire    kernel_row_done = (k_x_cntr == (kernel_width - 1));
-    wire    kernel_last_col = (k_y_cntr == (kernel_hight - 1));
+    wire    kernel_last_col = (k_y_cntr == (kernel_height - 1));
     wire    kernel_done     = (kernel_row_done && kernel_last_col);
     wire    di_row_done     = (di_x_cntr == di_x_stop) && kernel_done;
     wire    di_last_col     = (di_y_cntr == di_y_stop);
@@ -129,34 +132,34 @@ module conv #( parameter    DSIZE = 256,
         else                        di_y_cntr <= di_y_cntr;
 
 
-    assign di_addr  =   di_x_cntr   +   data_width      *   di_y_cntr;
+    wire [7:0] image_x = di_x_cntr + k_x_cntr;
+    wire [7:0] image_y = di_y_cntr + k_y_cntr;
+
+    assign di_addr  =   image_x   +   data_width    *   image_y;
     assign k_addr   =   k_x_cntr    +   kernel_width    *   k_y_cntr; 
-    
-    // The MAC with saturation
-    wire  [15:0]  mul;
-    assign        mul = DI[di_addr] *   kernel[k_addr];
-    
-    reg[15:0] acc;
-    always @(posedge clk, negedge rst_n) 
-        if(!rst_n) 
-            acc <= 0;
-        else if(kernel_done)
-            acc <= 0;
-        else
-            acc <= acc + mul;
+    wire [7:0] do_addr = di_x_cntr + data_width * di_y_cntr;
 
-
-// underflow and overflow
-    // check whether we are >127 or <-128
-    wire u =  acc[15] & (~&acc[15:7]);
-    wire o = ~acc[15] & (|acc[15:7]);
+    wire [7:0] temp_data = DI[di_addr];
+    wire [7:0] temp_kernel = kernel[k_addr*8];
+ 
+    wire [15:0] mul; 
+    assign mul = temp_data * temp_kernel;
+    
+    reg[15:0] acc=0;
         
-    // Change later
-    always @(posedge clk)
-        if(kernel_done)
-            DO[di_addr] <= u ? 8'h80 : o ? 8'd127 : acc;
-        else 
-            DO[di_addr] <= DO[di_addr];
+    always @(posedge clk, negedge rst_n) 
+    begin
+        if(!rst_n) 
+            acc = 0;
+        else if(state == CALC)
+            acc = acc + mul; 
+            if(kernel_done) begin 
+                DO[do_addr] = (acc[15] & (~&acc[15:7])) ? 8'h80 : (~acc[15] & (|acc[15:7])) ? 8'd127 : acc;
+                acc = 0;
+            end
+        else
+            acc = acc;
+    end
 
     assign done = di_done;
 
